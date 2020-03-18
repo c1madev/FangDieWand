c857351b476b1eaaf5a06825d84b03d5169e6208
Tobias Kunze <r@rixx.de>
2020-03-18 17:26:07 +0100

    onClick funktioniert, zeichneWand-Funktion

--- fangdiewand.js
+++ fangdiewand.js
@@ -1,21 +1,42 @@
+
+
+var Wall = [];
+var Field = [];
+var canvas = document.querySelector("canvas")
+
+const zeichneWand = (x, y, hoehe, breite, farbe) => {
+    let wand = new Path.Rectangle(new Point(x, y), new Size(hoehe, breite));
+    wand.strokeColor = farbe;
+    wand.strokeWidth = 5;
+    wand.onMouseEnter = function (event) {
+        canvas.style.cursor = "pointer";
+    }
+    wand.onMouseLeave = function (event) {
+        canvas.style.cursor = "default";
+    }
+    wand.onClick = function (event) {
+        event.currentTarget.strokeColor = "red";
+        this.bringToFront()
+    }
+    return wand
+}
+
 start = () => {
-    var canvas = document.querySelector("canvas")
+
     document.title = "Fang die Wand!"
 
     rahmenAussen = new Path.Rectangle(new Point(64, 64), new Size(830, 830));
     rahmenAussen.strokeColor = "black";
     rahmenAussen.strokeWidth = 15;
 
-    var Wall = [];
     var i = 0;
-    var Field = [];
 
     for (var breite = 88; breite < 800; breite = breite+100) {
         for (var hoehe = 88; hoehe < 800; hoehe = hoehe + 100) {
             Field[i] = new Path.Rectangle(new Point(breite, hoehe), new Size(90, 90));
             Field[i].fillColor = "white";
-            Field[i].onMouseEnter = function (event) {canvas.style.cursor = "pointer";}
-            Field[i].onMouseLeave = function (event) {canvas.style.cursor = "default";}
+            //Field[i].onMouseEnter = function (event) {canvas.style.cursor = "pointer";}
+            //Field[i].onMouseLeave = function (event) {canvas.style.cursor = "default";}
             Field[i].onclick = function (event) {
                 Field[i].strokeColor = "white";
                 Field[i].strokeWidth = 3;
@@ -25,29 +46,18 @@ start = () => {
         }
     }
 
-    i = 0;
 
-    for (var breite = 180; breite < 800; breite = breite + 100) {
-        for (var hoehe = 80; hoehe < 800; hoehe = hoehe + 100) {
-            Wall[i] = new Path.Rectangle(new Point(breite, hoehe), new Size(5, 100));
-            Wall[i].strokeColor = "#eaeaea";
-            Wall[i].strokeWidth = 5;
-            Wall[i].onMouseEnter = function (event) {canvas.style.cursor = "pointer";}
-            Wall[i].onMouseLeave = function (event) {canvas.style.cursor = "default";}
-            Wall[i].onclick = function (event) {
-                Wall[i].strokeColor = "red";
-                Wall[i].insertAbove();
+    for (var x = 0; x < 8; x++) {
+        for (var y = 0; y < 8; y++) {
+            if (y == 0) {
+                Wall[x] = []
             }
-            Wall[i+1] = new Path.Rectangle(new Point(hoehe, breite), new Size(100, 5));
-            Wall[i+1].strokeColor = "#eaeaea";
-            Wall[i+1].strokeWidth = 5;
-            Wall[i+1].onMouseEnter = function (event) {canvas.style.cursor = "pointer";}
-            Wall[i+1].onMouseLeave = function (event) {canvas.style.cursor = "default";}
-            Wall[i+1].onclick = function (event) {
-                Wall[i+1].strokeColor = "red";
-                Wall[i+1].insertAbove();
+            const punktX = 180 + 100*x
+            const punktY = 80 + 100*y
+            Wall[x][y] = {
+                hoch: zeichneWand(punktX, punktY, 5, 100, "#eaeaea"),
+                quer: zeichneWand(punktY, punktX, 100, 5, "#eaeaea"),
             }
-            i = i+2;
         }
     }
 
